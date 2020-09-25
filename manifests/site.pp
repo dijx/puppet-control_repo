23dcefe5f1385ce4574e6336ced5e4e20b1793ab
dijx <dijxie@gmail.com>
2020-09-25 14:28:20 +0200

    owner

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
   file { '/root/README':
-    ensure => file,
-    content => 'this is a readme\n',
+    ensure  => file,
+    content => 'this is a readme',
+    owner   => 'vagrant',
   }
 }
