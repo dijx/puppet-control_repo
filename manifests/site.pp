43edc95d594c2460f071111c91166cd08479c54e
dijx <dijxie@gmail.com>
2020-09-28 13:27:45 +0200

    master node definition

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,6 @@
 node default {
-  file { '/root/README':
-    ensure  => file,
-    content => 'this is a readme',
-    owner   => 'root',
-    group   => 'users',
-  }
+}
 
-  file { '/home/vagrant/README':
-    ensure  => file,
-    content => 'this is a readme',
-    owner   => 'vagrant',
-    group   => 'vagrant',
-  }
+node 'master.puppet.vm' {
+  include role::master_server
 }
